0e57f68b619396dec5c69666456a0078a24a0c1f
phurlocker <40524872+PAT-HURLCOKER@users.noreply.github.com>
2018-08-14 17:25:48 -0700

    Add files via upload

---
+++ NewsMood/config.py
@@ -0,0 +1,8 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "FZwZBQcQjG4p73LYx2wW7G4MM"
+consumer_secret = "BfwU6w65eqhriEENvTmcfJWlvsTSgYPBAr7JZtczcgYBOiRMaI"
+access_token = "1026353940604932096-X3VS3DREVJtVxhQG1PMlElH1Bwqgl5"
+access_token_secret = "J6Ozg1mTWmFi9MHB5hKVZzwt19Kp9tlZXbksVFTSJGodh"
+
+weather_api_key = "2bb27b1c9b5f2cc6497ad9ad315668e5"
